dd0e0d1157461120cf763d5344df30ed69bae95f
YogliB <yogev.boaron-ben-har@skai.io>
2022-04-13 22:48:42 +0300

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				message: error.message,
+				status: status,
+			},
+		};
+	}
+</script>
+
+<script>
+	export let message;
+	export let status;
+</script>
+
+{#if status == 404}
+	404
+{:else}
+	{message} {status}
+{/if}
