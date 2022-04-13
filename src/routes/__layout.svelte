976785a8141a724dcc165bed407ba479ab3c5d43
YogliB <yogev.boaron-ben-har@skai.io>
2022-04-13 22:48:18 +0300

    add sentry & error page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
-<script>
-	import { browser } from '$app/env';
+<script lang="ts">
 	import 'virtual:windi.css';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
 
-	if (browser) import('virtual:windi-devtools');
+	let Sentry;
+
+	onMount(async () => {
+		if (browser) {
+			import('virtual:windi-devtools');
+			Sentry = (await import('@sentry/browser')).default;
+
+			Sentry.init({});
+		}
+	});
 </script>
 
 <slot />
