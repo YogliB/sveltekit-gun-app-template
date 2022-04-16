3af2f1b8b437de97c33d4ee5bb24e76ddd0b5743
YogliB <yogev.boaron-ben-har@skai.io>
2022-04-16 19:39:22 +0300

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 import WindiCSS from 'vite-plugin-windicss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 		vite: {
 			plugins: [WindiCSS()],
 		},
